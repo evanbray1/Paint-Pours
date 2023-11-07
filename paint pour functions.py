269e308ff66ff23c760928e92ca8443225551224
evanbray1 <131594924+evanbray1@users.noreply.github.com>
2023-11-06 19:55:37 -0500

    small update

--- paint pour functions.py
+++
@@ -1,7 +0,0 @@
-from numba import njit
-
-#Define a function for interpolating between two points, which we do a lot here. This is a convenient one because it doesn't have "kinks" at the endpoints like a linear interpolation function would.
-#https://en.wikipedia.org/wiki/Smoothstep
-@njit(parallel=True,fastmath=True)   #Like magic, the @njit bit makes the below function run faster by converting it into machine code.
-def smootherstep_function(x):
-    return 6*x**5-15*x**4+10*x**3
